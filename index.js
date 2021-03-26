a233da987323791ae894f2b212f7d02e949d1e91
shafiimam <shafi15-8072@diu.edu.bd>
2021-03-26 09:26:14 +0600

    cors installed

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ const express = require('express')
 const app = express()
 
 // const rootCall = (req,res)=>res.send('thank you very much');
+
+const users= [
+    "asad","mahin","shuvo","joy","piash"
+]
+
 app.get('/',(req,res)=>{
-    res.send('I know how to open node!! yeeey')
-} )
+    const fruit={
+        product: 'ada',
+        price: '220',
+    }
+    res.send(fruit)
+} );
+app.get('/fruits/banana',(req,res)=>{
+     res.send({fruit:'banana',price:'20'});
+ })
+
+ app.get('/users/:id',(req,res)=>{
+   const id = req.params.id;
+   const name = users[id]
+   console.log(req.query)
+   res.send({id,name});
+ })
 app.listen(3000, ()=> console.log('listening to port 3000'))
